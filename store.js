25252987d0e26780d089be2ae226343c2e140e1c
alissondemidio <68760800+azzb10@users.noreply.github.com>
2022-01-11 20:26:31 -0300

    Update store.js

--- store.js
+++ store.js
@@ -4,13 +4,24 @@ const reducer = (state, update) => merge(state, update)
 
 let state = {}
 
-state = reducer(state, {foo: "foo"})
-state = reducer(state, {bar: "bar"})
-state = reducer(state, {foo: "baz"})
-
 class Store {
   constructor(reducer, initialState) {
     this.reducer = reducer
     this.state = initialState
   }
+
+  getState() {
+    return this.state
+  }
+
+  dispatch(update){
+    this.state = this.reducer(this.state, update)
+  }
 }
+const store = new Store(reducer, {})
+
+store.dispatch({foo: "foo"})
+store.dispatch({bar: "bar"})
+store.dispatch({foo: "baz"})
+
+console.log(store.getState())
