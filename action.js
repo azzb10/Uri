1bf1bb1f81a86487271ebc00c2020121af362843
alissondemidio <68760800+azzb10@users.noreply.github.com>
2022-01-12 07:21:06 -0300

    Update action.js

--- action.js
+++ action.js
@@ -8,20 +8,24 @@ class Store {
     return this.state
   }
 
-  dispatch(update){
+  dispatch(update) {
     this.state = this.reducer(this.state, update)
   }
 }
+
+const DEFAULT_STATE = {user: {}, contacts: []}
 const merge = (prev, next) => Object.assign({}, prev, next)
 const userReducer = (state, update) => merge(state, update)
 
 const reducer = (state, action) => {
-  if(action === 'UPDATE_USER'){
+  if(action.type === 'UPDATE_USER'){
     merge(state, {user: userReducer(state.user, action.payload)})
   }
+
+  return state
 }
 
-const store = new Store(reducer, {})
+const store = new Store(reducer, DEFAULT_STATE)
 
 store.dispatch({type: 'UPDATE_USER', payload: {foo: "foo"}})
 store.dispatch({type: 'UPDATE_USER', payload: {bar: "bar"}})
