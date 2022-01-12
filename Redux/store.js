4ecb7c94343f9ae27879ea0298233e10d6de4db7
alissondemidio <68760800+azzb10@users.noreply.github.com>
2022-01-12 08:56:54 -0300

    Create store.js

---
+++ Redux/store.js
@@ -0,0 +1,14 @@
+import {createStore} from redux;
+//const {createStore, combineReducers} = require('redux')
+import reducer from './reducer'
+
+const store = createStore(reducer, DEFAULT_STATE)
+
+store.dispatch(updateUser({foo: "foo"}))
+store.dispatch(updateUser({bar: "bar"}))
+store.dispatch(updateUser({foo: "baz"}))
+
+store.dispatch(addContact({name: 'MyName', number: '12345687'}))
+store.dispatch(addContact({name: 'YourName', number: '5678934567'}))
+
+console.log(store.getState())
